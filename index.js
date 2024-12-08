fd671c06ecb9f666ccb57fe706fc7d2716ed4ba5
kirkrieberger <rieberg1@ualberta.ca>
2024-12-07 22:45:13 -0700

    Clear fields on failed login and "enter" key detection

--- index.js
+++ index.js
@@ -3,26 +3,49 @@
     const loginModal = new bootstrap.Modal('#loginModal');
     loginModal.show();
 
-    document.getElementById('loginButton').addEventListener('click', function () {
-        let adr = $("#address").val();
-        let key = $("#key").val();
-        let port = 7777;
-        response = pywebview.api.login(adr, key, port);
-
-        response.then(value => {
-            showResponse(value);
-            loginModal.hide();
-        }).catch(error => {
-            showResponse(error);
-        });
-        // Error
-        if (response == 523) {
-            // Bad address
-
+    $('#loginButton').on('click', function () {
+        login();
+    });
+    $('#address').on('keypress', (event) => {
+        if (event.key === "Enter") {
+            login();
         }
     });
-});
+    $('#key').on('keypress', (event) => {
+        if (event.key === "Enter") {
+            login();
+        }
+    });
+ });
+
+function login() {
+    let adr = $("#address").val();
+    let key = $("#key").val();
+    let port = 7777;
+    response = pywebview.api.login(adr, key, port);
+
+    response.then(value => {
+        loginModal.hide();
+    }).catch(error => {
+        showResponse(error);
+        // Clear fields
+        $("#address").val("");
+        $("#key").val("");
+    });
+}
 
 function showResponse(response) {
     alert(response);
+}
+
+function showDash() {
+
+}
+
+function showSettings() {
+
+}
+
+function showConsole() {
+
 }
