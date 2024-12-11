2ae2eed626c40e967d8241a25ee34c8ba80a52b2
kirkrieberger <kirk@rieberger.ca>
2024-12-10 22:48:03 -0700

    Pull server state update into bespoke function

--- window.py
+++ window.py
@@ -7,10 +7,6 @@ def loop(window: webview.Window):
     # Setup visual fields
     serverName = window.dom.get_element("#server")
     sessionName = window.dom.get_element("#session")
-    statusRun = window.dom.get_element("#run")
-    statusStart = window.dom.get_element("#start")
-    statusPause = window.dom.get_element("#pause")
-    statusStop = window.dom.get_element("#stop")
     phase = window.dom.get_element("#phase")
     tier = window.dom.get_element("#tier")
     schematic = window.dom.get_element("#schem")
@@ -21,7 +17,7 @@ def loop(window: webview.Window):
     # Window Update Loop
     while True:  # TODO: Use LW Query to determine if update is needed
         if server.loggedIn:
-            server.pollServerState()
+            server.pollServerState(window, server.serverState)
             serverName.text = server.serverName
             phase.text = server.gamePhase
             sessionName.text = server.sessionName
@@ -30,32 +26,32 @@ def loop(window: webview.Window):
             playerCount.text = server.numPlayers
             maxCount.text = server.maxPlayers
             tickRate.text = server.tickRate
-            match server.serverState:
-                case "idle":
-                    statusRun.classes.append("d-none")
-                    statusStart.classes.append("d-none")
-                    statusPause.classes.append("d-none")
-                    statusStop.classes.remove("d-none")
-                case "loading":
-                    statusRun.classes.append("d-none")
-                    statusStart.classes.remove("d-none")
-                    statusPause.classes.append("d-none")
-                    statusStop.classes.append("d-none")
-                case "playing":
-                    if server.paused:
-                        statusRun.classes.append("d-none")
-                        statusStart.classes.append("d-none")
-                        statusPause.classes.remove("d-none")
-                        statusStop.classes.append("d-none")
-                    else:
-                        statusRun.classes.remove("d-none")
-                        statusStart.classes.append("d-none")
-                        statusPause.classes.append("d-none")
-                        statusStop.classes.append("d-none")
 
             sleep(10)
         else:
             sleep(1)
+
+
+def updateStatusDisp(window: webview.Window, newState: str) -> None:
+    statusRun = window.dom.get_element("#run")
+    statusStart = window.dom.get_element("#start")
+    statusPause = window.dom.get_element("#pause")
+    statusStop = window.dom.get_element("#stop")
+
+    statusRun.classes.append("d-none")
+    statusStart.classes.append("d-none")
+    statusPause.classes.append("d-none")
+    statusStop.classes.append("d-none")
+    match newState:
+        case "idle":
+            statusStop.classes.remove("d-none")
+        case "loading":
+            statusStart.classes.remove("d-none")
+        case "playing":
+            if server.paused:
+                statusPause.classes.remove("d-none")
+            else:
+                statusRun.classes.remove("d-none")
 
 
 if __name__ == "__main__":
