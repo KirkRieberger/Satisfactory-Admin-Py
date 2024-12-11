9d89a5efbb536adbbbf34bcaf8229ad117d5b261
kirkrieberger <rieberg1@ualberta.ca>
2024-12-10 19:30:38 -0700

    Add tab change logic, make loginModal global

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const loginModal = new bootstrap.Modal('#loginModal');
+ 
  // Wait for Python API to be initialized
  window.addEventListener('pywebviewready', function () {
-    const loginModal = new bootstrap.Modal('#loginModal');
     loginModal.show();
+
+    // Login modal submit listeners
 
     $('#loginButton').on('click', function () {
         login();
     });
+
     $('#address').on('keypress', (event) => {
         if (event.key === "Enter") {
             login();
         }
     });
+
     $('#key').on('keypress', (event) => {
         if (event.key === "Enter") {
             login();
@@ -22,7 +27,7 @@ function login() {
     let adr = $("#address").val();
     let key = $("#key").val();
     let port = 7777;
-    response = pywebview.api.login(adr, key, port);
+    let response = pywebview.api.login(adr, key, port);
 
     response.then(value => {
         loginModal.hide();
@@ -39,13 +44,31 @@ function showResponse(response) {
 }
 
 function showDash() {
+    $('#dashboard').removeClass("d-none");
+    $('#settings').addClass("d-none");
+    $('#console').addClass("d-none");
 
+    $('#dashLink').addClass("active");
+    $('#settingsLink').removeClass("active");
+    $('#consoleLink').removeClass("active");
 }
 
 function showSettings() {
+    $('#dashboard').addClass("d-none");
+    $('#settings').removeClass("d-none");
+    $('#console').addClass("d-none");
 
+    $('#dashLink').removeClass("active");
+    $('#settingsLink').addClass("active");
+    $('#consoleLink').removeClass("active");
 }
 
 function showConsole() {
+    $('#dashboard').addClass("d-none");
+    $('#settings').addClass("d-none");
+    $('#console').removeClass("d-none");
 
+    $('#dashLink').removeClass("active");
+    $('#settingsLink').removeClass("active");
+    $('#consoleLink').addClass("active");
 }
