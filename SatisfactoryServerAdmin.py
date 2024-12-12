283aec630887c3b04ae034e913cecdc9d614eac0
kirkrieberger <kirk@rieberger.ca>
2024-12-11 23:50:46 -0700

    Update login function to receive string port. Add TODO

--- SatisfactoryServerAdmin.py
+++ SatisfactoryServerAdmin.py
@@ -221,7 +221,7 @@ class SatisfactoryServerAdmin:
         """
         return f"SatisfactoryServer('{self.address}', '{self.key}')"
 
-    def login(self, ip: str = None, token: str = None, port: int = 7777) -> int:
+    def login(self, ip: str = None, token: str = None, port: str = "7777") -> int:
         """TODO: _summary_
 
         Args:
@@ -238,6 +238,8 @@ class SatisfactoryServerAdmin:
         """
         self.ip = ip
         self.port = port
+
+        # TODO: Regex to verify valid FQDN or IP
         self.address = "https://" + ip + ":" + str(port) + "/api/v1"
         self.logger.info(f"Connecting to {self.address}...")
         # Split token into payload and key
