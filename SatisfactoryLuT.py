4983e45c7c387717c1c36b4c8c7b13cc04ff6da4
kirkrieberger <kirk@rieberger.ca>
2024-12-13 23:46:31 -0700

    Factor ugly LuTs to separate module

---
+++ SatisfactoryLuT.py
@@ -0,0 +1,89 @@
+# Tables to look up from
+prettyPhase = {
+    "/Script/FactoryGame.FGGamePhase'/Game/FactoryGame/GamePhases/GP_Project_Assembly_Phase_1.GP_Project_Assembly_Phase_1'": "Distribution Platform",
+    "/Script/FactoryGame.FGGamePhase'/Game/FactoryGame/GamePhases/GP_Project_Assembly_Phase_2.GP_Project_Assembly_Phase_2'": "Construction Dock",
+    "/Script/FactoryGame.FGGamePhase'/Game/FactoryGame/GamePhases/GP_Project_Assembly_Phase_3.GP_Project_Assembly_Phase_3'": "Main Body",
+    "/Script/FactoryGame.FGGamePhase'/Game/FactoryGame/GamePhases/GP_Project_Assembly_Phase_4.GP_Project_Assembly_Phase_4'": "Propulsion",
+    "/Script/FactoryGame.FGGamePhase'/Game/FactoryGame/GamePhases/GP_Project_Assembly_Phase_5.GP_Project_Assembly_Phase_5'": "Assembly",
+}
+# This LUT hurts me
+prettySchematic = {
+    # Tier 0 - Onboarding
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_0-1.Schematic_0-1_C'": "HUB Upgrade 1",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_0-2.Schematic_0-2_C'": "HUB Upgrade 2",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_0-3.Schematic_0-3_C'": "HUB Upgrade 3",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_0-4.Schematic_0-4_C'": "HUB Upgrade 4",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_0-5.Schematic_0-5_C'": "HUB Upgrade 5",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_0-6.Schematic_0-6_C'": "HUB Upgrade 6",
+    # Phase 0 - Tier 1
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_1-1.Schematic_1-1_C'": "Base Building",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_1-2.Schematic_1-2_C'": "Logistics",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_1-3.Schematic_1-3_C'": "Field Research",
+    # Phase 0 - Tier 2
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_2-1.Schematic_2-1_C'": "Part Assembly",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_2-2.Schematic_2-2_C'": "Obstacle Clearing",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_2-3.Schematic_2-3_C'": "Jump Pads",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_2-4.Schematic_2-4_C'": "Resource Sink Bonus Program",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_2-5.Schematic_2-5_C'": "Logistics Mk.2",
+    # Phase 1 - Tier 3 - Distribution Platform
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_3-1.Schematic_3-1_C'": "Coal Power",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_3-2.Schematic_3-2_C'": "Vehicular Transport",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_3-3.Schematic_3-3_C'": "Basic Steel Production",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_3-4.Schematic_3-4_C'": "Enhanced Asset Security",
+    # Phase 1 - Tier 4 - Distribution Platform
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_4-1.Schematic_4-1_C'": "FICSIT Blueprints",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_4-2.Schematic_4-2_C'": "Logistics Mk.3",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_4-3.Schematic_4-3_C'": "Advanced Steel Production",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_4-4.Schematic_4-4_C'": "Expanded Power Infrastructure",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_4-5.Schematic_4-5_C'": "Hypertubes",
+    # Phase 2 - Tier 5 - Construction Dock
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_5-1.Schematic_5-1_C'": "Jetpack",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_5-2.Schematic_5-2_C'": "Oil Processing",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_5-3.Schematic_5-3_C'": "Logistics Mk.4",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_5-4.Schematic_5-4_C'": "Fluid Packaging",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_5-5.Schematic_5-5_C'": "Petroleum Power",
+    # Phase 2 - Tier 6 - Construction Dock
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_6-1.Schematic_6-1_C'": "Industrial Manufacturing",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_6-2.Schematic_6-2_C'": "Monorail Train Technology",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_6-3.Schematic_6-3_C'": "Railway Signaling",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_6-4.Schematic_6-4_C'": "Pipeline Engineering Mk.2",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_6-5.Schematic_6-5_C'": "FICSIT Blueprints Mk.2",
+    # Phase 3 - Tier 7 - Main Body
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_7-1.Schematic_7-1_C'": "Bauxite Refinement",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_7-2.Schematic_7-2_C'": "Hoverpack",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_7-3.Schematic_7-3_C'": "Logistics Mk.5",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_7-4.Schematic_7-4_C'": "Hazmat Suit",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_7-5.Schematic_7-5_C'": "Control System Development",
+    # Phase 3 - Tier 8 - Main Body
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_8-1.Schematic_8-1_C'": "Aeronautical Engineering",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_8-2.Schematic_8-2_C'": "Nuclear Power",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_8-3.Schematic_8-3_C'": "Advanced Aluminum Production",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_8-4.Schematic_8-4_C'": "Leading-edge Production",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_8-5.Schematic_8-5_C'": "Particle Enrichment",
+    # Phase 4 - Tier 9 - Propulsion
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_9-1.Schematic_9-1_C'": "Matter Conversion",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_9-2.Schematic_9-2_C'": "Quantum Encoding",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_9-3.Schematic_9-3_C'": "Ficsit Blueprints Mk.3",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_9-4.Schematic_9-4_C'": "Spatial Energy Regulation",
+    "/Script/Engine.BlueprintGeneratedClass'/Game/FactoryGame/Schematics/Progression/Schematic_9-5.Schematic_9-5_C'": "Peak Efficiency",
+}
+
+
+def getSchematic(serverSchematic: str):
+    """
+        docstring
+    """
+    if serverSchematic in prettySchematic:
+        return prettySchematic[serverSchematic]
+    else:
+        return None
+
+
+def getPhase(serverPhase: str):
+    """
+        docstring
+    """
+    if serverPhase in prettyPhase:
+        return prettyPhase[serverPhase]
+    else:
+        return None
